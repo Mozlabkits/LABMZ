cbd59c92806ca6b98c02391ce80b54fb6f277a7c
Mozlabkits <tavaresnamalope@gmail.com>
2026-08-28 13:03:23 +0200

    Corrigir Service Worker para PWABuilder

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,92 @@
-const CACHE_NAME = "labmz-v2";
+const CACHE_NAME = "LABMZ-v3";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./icons/icon.svg"
+const APP_SHELL = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+/* INSTALAÇÃO */
+self.addEventListener("install", function (event) {
 
-  self.skipWaiting();
+    event.waitUntil(
+
+        caches.open(CACHE_NAME).then(function (cache) {
+
+            return cache.addAll(APP_SHELL);
+
+        })
+
+    );
+
+    self.skipWaiting();
+
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName))
-      );
-    })
-  );
 
-  self.clients.claim();
+/* ATIVAÇÃO */
+self.addEventListener("activate", function (event) {
+
+    event.waitUntil(
+
+        caches.keys().then(function (cacheNames) {
+
+            return Promise.all(
+
+                cacheNames.map(function (cacheName) {
+
+                    if (cacheName !== CACHE_NAME) {
+
+                        return caches.delete(cacheName);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+
+/* FUNCIONAMENTO OFFLINE */
+self.addEventListener("fetch", function (event) {
+
+    if (event.request.method !== "GET") {
+
+        return;
+
+    }
+
+    event.respondWith(
+
+        caches.match(event.request).then(function (cachedResponse) {
+
+            if (cachedResponse) {
+
+                return cachedResponse;
+
+            }
+
+            return fetch(event.request).then(function (networkResponse) {
+
+                return networkResponse;
+
+            }).catch(function () {
+
+                return caches.match("./index.html");
+
+            });
+
+        })
+
+    );
+
 });
