4d406d0f6d5d73159c2a9e3606b373f018cae3e7
Mozlabkits <tavaresnamalope@gmail.com>
2026-08-28 11:33:31 +0200

    Update console log message in app.js

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-﻿// ==========================================
-// LABMZ - Laboratório de Aprendizagem
-// de Biologia e Química de Moçambique
-// ==========================================
-
 document.addEventListener("DOMContentLoaded", function () {
 
-```
-console.log("LABMZ carregado com sucesso!");
-```
+    console.log("[LABMZ] Aplicação carregada corretamente.");
 
 });
